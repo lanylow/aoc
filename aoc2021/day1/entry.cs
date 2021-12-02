33d39d58825212f6d3898b325ff86f30be970274
lanylow <lanylow@protonmail.com>
2021-12-02 14:18:51 +0100

    2021 Day 1

---
+++ aoc2021/day1/entry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace day1 {
+  internal class entry {
+    static int get_incresed_measurements(int[] measurements, int window) {
+      var incresed = 0;
+      for (int i = window; i < measurements.Length; i++) {
+        var last_sum = 0;
+        var previous_sum = 0;
+
+        for (int j = 0; j < window; j++) {
+          last_sum += measurements[i - j];
+          previous_sum += measurements[i - j - 1];
+        }
+
+        if (last_sum > previous_sum)
+          incresed++;
+      }
+      return incresed;
+    }
+
+    static void Main(string[] args) {
+      var all_lines = File.ReadAllLines("input.txt");
+      var measurements = new int[all_lines.Length];
+
+      for (var i = 0; i < all_lines.Length; i++)
+        measurements[i] = int.Parse(all_lines[i]);
+
+      Console.WriteLine("Increased measurements (Part 1): " + get_incresed_measurements(measurements, 1));
+      Console.WriteLine("Sums larger than the previous sum (Part 2): " + get_incresed_measurements(measurements, 3));
+
+      Console.ReadLine();
+    }
+  }
+}
