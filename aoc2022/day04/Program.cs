52184cf3987c30e541b1386b1d418fd9976c65e1
lanylow <lanylow@protonmail.com>
2023-11-18 23:33:43 +0100

    2022 Day 4

---
+++ aoc2022/day04/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace day04;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var (partOne, partTwo) = Solve();
+        
+        Console.WriteLine($"Part 1: {partOne}");
+        Console.WriteLine($"Part 2: {partTwo}");
+    }
+
+    private static (int, int) Solve()
+    {
+        var assignments = File.ReadAllLines("input.txt");
+        
+        var fullyContained = 0;
+        var overlapping = 0;
+        
+        foreach (var assignment in assignments)
+        {
+            var (r1, r2) = ParseLine(assignment);
+
+            if (r1.FullyContains(r2))
+            {
+                fullyContained++;
+            }
+
+            if (r1.Overlaps(r2))
+            {
+                overlapping++;
+            }
+        }
+
+        return (fullyContained, overlapping);
+    }
+
+    private static (Range, Range) ParseLine(string str)
+    {
+        var split = str.Trim().Split(',');
+        return (Range.FromString(split[0]), Range.FromString(split[1]));
+    }
+}
---
+++ aoc2022/day04/Range.cs
@@ -0,0 +1,31 @@
+﻿namespace day04;
+
+public class Range
+{
+    private int Start { get; }
+    private int End { get; }
+
+    private Range(int start, int end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public static Range FromString(string str)
+    {
+        var split = str.Trim().Split('-');
+        return new Range(int.Parse(split[0]), int.Parse(split[1]));
+    }
+
+    public bool FullyContains(Range other)
+    {
+        return (other.Start >= Start && other.End <= End) || (Start >= other.Start && End <= other.End);
+    }
+
+    public bool Overlaps(Range other)
+    {
+        var r1 = Start < other.Start ? this : other;
+        var r2 = Start > other.Start ? this : other;
+        return r1.End >= r2.Start;
+    }
+}
