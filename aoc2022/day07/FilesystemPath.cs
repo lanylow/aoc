c2ecb013d99d2ef5c96cf886b9d34d1192511c3f
lanylow <lanylow@protonmail.com>
2023-11-19 15:31:19 +0100

    2022 Day 7

---
+++ aoc2022/day07/FilesystemPath.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace day07;
+
+public class FilesystemPath
+{
+    private StringBuilder StringBuilder { get; } = new();
+
+    private FilesystemPath(string path)
+    {
+        StringBuilder = new StringBuilder(path);
+    }
+
+    public FilesystemPath()
+    {
+        
+    }
+    
+    public void In(string name)
+    {
+        if (name == "/")
+        {
+            StringBuilder.Clear();
+        }
+        else
+        {
+            StringBuilder.Append($"/{name}");
+        }
+    }
+
+    public bool Out()
+    {
+        if (StringBuilder.Length <= 0)
+        {
+            return false;
+        }
+        
+        var lastDirectory = StringBuilder.ToString().LastIndexOf("/", StringComparison.Ordinal);
+        StringBuilder.Remove(lastDirectory, StringBuilder.Length - lastDirectory);
+        return true;
+    }
+
+    public FilesystemPath Clone()
+    {
+        return new FilesystemPath(StringBuilder.ToString());
+    }
+    
+    public override string ToString()
+    {
+        var str = StringBuilder.ToString();
+        return str == "" ? "/" : str;
+    }
+}
---
+++ aoc2022/day07/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace day07;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = ParseInput().ToList();
+        
+        Console.WriteLine($"Part 1: {SolvePartOne(input)}");
+        Console.WriteLine($"Part 1: {SolvePartTwo(input)}");
+    }
+
+    private static int SolvePartOne(IEnumerable<int> input)
+    {
+        return input.Where(x => x <= 100000).Sum();
+    }
+
+    private static int SolvePartTwo(IReadOnlyCollection<int> input)
+    {
+        const int total = 70000000;
+        const int required = 30000000;
+        
+        var used = input.Last();
+        var unused = total - used;
+
+        return input.Where(x => x + unused >= required).Take(1).First();
+    }
+    
+    private static IEnumerable<int> ParseInput()
+    {
+        var input = File.ReadAllLines("input.txt");
+        var sizeMap = new Dictionary<string, int>();
+        var path = new FilesystemPath();
+
+        foreach (var line in input)
+        {
+            if (line.StartsWith("$ cd "))
+            {
+                var directoryName = line.Split(' ')[2];
+
+                if (directoryName == "..")
+                {
+                    path.Out();
+                }
+                else
+                {
+                    path.In(directoryName);
+                }
+            }
+            else if (line.StartsWith("$ ls") || line.StartsWith("dir "))
+            {
+                
+            }
+            else
+            {
+                var size = int.Parse(line.Split(' ')[0]);
+                var clone = path.Clone();
+
+                while (true)
+                {
+                    var pathStr = clone.ToString();
+                    
+                    if (!sizeMap.ContainsKey(pathStr))
+                    {
+                        sizeMap[pathStr] = size;
+                    }
+                    else
+                    {
+                        sizeMap[pathStr] += size;
+                    }
+
+                    if (!clone.Out())
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        var output = sizeMap.Values.ToList();
+        output.Sort();
+        return output;
+    }
+}
