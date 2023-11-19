ce6b1d29ac5481d45d2aafd63b8f887db3a6beae
lanylow <lanylow@protonmail.com>
2023-11-19 14:24:44 +0100

    2022 Day 6

---
+++ aoc2022/day06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace day06;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var packet = File.ReadAllText("input.txt");
+     
+        Console.WriteLine($"Part 1: {FindStartOfPacket(packet, 4)}");
+        Console.WriteLine($"Part 2: {FindStartOfPacket(packet, 14)}");
+    }
+
+    private static int FindStartOfPacket(string str, int unique)
+    {
+        for (var i = unique; i < str.Length; i++)
+        {
+            var subString = str.Substring(i - unique, unique);
+            var hashSet = new HashSet<char>(subString);
+            
+            if (hashSet.Count == unique)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
