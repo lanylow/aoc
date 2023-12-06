077ea63cf94a674a117fde131c3a9be8dcdb98a2
lanylow <31806776+lanylow@users.noreply.github.com>
2023-12-06 17:40:57 +0100

    2023 Day 6

---
+++ aoc2023/day06/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace day06;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+        
+        Console.WriteLine($"Part 1: {SolvePartOne(input)}");
+        Console.WriteLine($"Part 2: {SolvePartTwo(input)}");
+    }
+
+    private static long SolvePartOne(IReadOnlyList<string> input) => input[0]
+        .Split(" ")
+        .Skip(1)
+        .Where(x => x.Trim() != string.Empty)
+        .Select(long.Parse)
+        .Zip(input[1]
+            .Split(" ")
+            .Skip(1)
+            .Where(x => x.Trim() != string.Empty)
+            .Select(long.Parse))
+        .Select(x => GetRecordTime(x.First, x.Second))
+        .Aggregate(1L, (x, y) => x * y);
+
+    private static long SolvePartTwo(IReadOnlyList<string> input) => GetRecordTime(CombineNumbers(input[0]), CombineNumbers(input[1]));
+
+    private static long CombineNumbers(string str) => long.Parse(str.Split(" ").Skip(1).Where(x => x.Trim() != string.Empty).Aggregate("", (x, y) => x + y));
+    
+    private static long GetRecordTime(long time, long distance)
+    {
+        var a = (double)time / 2;
+        var b = Math.Sqrt(a * a - distance);
+        return (long)(Math.Ceiling(a + b - 1) - Math.Floor(a - b + 1) + 1);
+    }
+}
