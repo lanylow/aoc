aac19febd77dd9e28a0d2273f8a2b302cacaa340
lanylow <lanylow@protonmail.com>
2023-12-08 15:11:19 +0100

    2023 Day 8

---
+++ aoc2023/day08/Network.cs
@@ -0,0 +1,15 @@
+﻿namespace day08;
+
+public class Network
+{
+    public List<bool> Instructions { get; }
+    public Dictionary<string, (string, string)> Table { get; }
+
+    public Network(List<bool> instructions, Dictionary<string, (string, string)> table)
+    {
+        Instructions = instructions;
+        Table = table;
+    }
+
+    public string Next(string start) => Instructions.Aggregate(start, (x, y) => y ? Table[x].Item1 : Table[x].Item2);
+}
---
+++ aoc2023/day08/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace day08;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = ParseInput();
+        
+        Console.WriteLine($"Part 1: {SolvePartOne(input)}");
+        Console.WriteLine($"Part 2: {SolvePartTwo(input)}");
+    }
+
+    private static long SolvePartOne(Network network) => network.Instructions.Count * Unfold("AAA", network.Next).TakeWhile(x => x != "ZZZ").Count();
+
+    private static long SolvePartTwo(Network network) => network.Instructions.Count * network.Table.Keys
+        .Where(x => x.EndsWith("A"))
+        .Select(x => 
+        {
+            var (index, end) = Unfold(x, network.Next).Select((end, index) => (index, end)).FirstOrDefault(y => y.end.EndsWith("Z"));
+
+            if (network.Next(x) == network.Next(end))
+            {
+                return index;
+            }
+            
+            return -1L;
+        })
+        .Where(x => x != -1)
+        .Aggregate(1L, GetLowestCommonMultiple);
+    
+    private static Network ParseInput()
+    {
+        var input = File.ReadAllLines("input.txt");
+
+        var instructions = input[0].Select(x =>
+        {
+            return x switch
+            {
+                'L' => true,
+                'R' => false,
+                _ => throw new ArgumentOutOfRangeException(nameof(x), x, null)
+            };
+        }).ToList();
+
+        var table = input.Skip(2).Select(x =>
+        {
+            var y = x.Split(" = ", 2);
+            var z = y[1].Split(", ", 2);
+            return (y[0], (z[0].Replace("(", ""), z[1].Replace(")", "")));
+        }).ToDictionary(x => x.Item1, x => x.Item2);
+
+        return new Network(instructions, table);
+    }
+
+    private static long GetGreatestCommonDivisor(long x, long y)
+    {
+        while (!y.Equals(default))
+        {
+            var t = y;
+            y = x % y;
+            x = t;
+        }
+
+        return x;
+    }
+
+    private static long GetLowestCommonMultiple(long x, long y)
+    {
+        return x / GetGreatestCommonDivisor(x, y) * y;
+    }
+
+    private static IEnumerable<T> Unfold<T>(T seed, Func<T, T> generator)
+    {
+        var current = seed;
+
+        while (true)
+        {
+            yield return current;
+            current = generator(current);
+        }
+    }
+}
